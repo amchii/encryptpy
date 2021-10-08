e47da256eb0c3a6a8431e1f8fd10e719cb310fcc
amchii <finethankuandyou@gmail.com>
2021-10-08 20:51:39 +0800

    Remove README_CN.md link on PyPI long description

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+import os
+
 from setuptools import find_packages, setup
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(base_dir, "README.md"), encoding="utf-8") as fp:
+    fp.readline()  # remove README_CN.md link on PyPI
+    long_description = fp.read()
 
 setup(
     packages=find_packages(),
     include_package_data=True,
     python_requires=">=3.6",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=["Cython", "Click", "GitPython~=3.0"],
     entry_points={
         "console_scripts": [
